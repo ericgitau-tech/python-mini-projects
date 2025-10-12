6f21268f3b4496850e7a016703db230358411679
ericgitau-tech <gitaueric09@gmail.com>
2025-10-12 19:04:55 +0300

    added compound Interest calculator

--- compoundInterestCalculator/main.py
+++ compoundInterestCalculator/main.py
@@ -1,3 +1,5 @@
+
+
 principle = 0
 rate = 0
 time = 0
@@ -5,17 +7,17 @@ time = 0
 while principle <= 0:
     principle = float(input("Enter the principle amount: "))
     if principle <= 0:
-        print("principle can't be less than or equal to zero")
+        print("Principle can't be less than or equal to zero")
 
 while rate <= 0:
-    rate = float(input("Enter the rate amount: "))
+    rate = float(input("Enter the interest rate: "))
     if rate <= 0:
         print("rate can't be less than or equal to zero")
-while time <= 0:
-    time = int(input("Enter the time yr"))
-    if time <= 0:
-        print("time can't be less than or equal to zero: ")
 
+while time <= 0:
+    time = int(input("Enter the time in yrs: "))
+    if time <= 0:
+        print("time can't be less than or equal to zero")
 total = principle * pow((1 + rate / 100), time)
 
-print(f"Balance after {time} year/s: ${total:.2f}")
+print(f"Balance after {time} years ${total:.2f}")
